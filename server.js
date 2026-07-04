496003b22a7e67eab36a5b44386fb45160ca9047
Aman <amanchdurga086@gmail.com>
2026-07-04 17:00:03 +0530

    Add MongoDB configuration and database connection

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const app = require("./app");
 
+require("dotenv").config();
+const connectDB = require("./config/db");
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+async function startServer() {
+  await connectDB();
+
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
+  
+};
+
+startServer();
